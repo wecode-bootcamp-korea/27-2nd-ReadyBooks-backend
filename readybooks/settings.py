692ce64ca3e690e760334aba8eb265e0fa1b484e
jinsung Park <jinsungpark@jinsungui-MacBookPro.local>
2021-12-13 17:33:09 +0900

    initial settings

--- readybooks/settings.py
+++ readybooks/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders'
+    'corsheaders',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
