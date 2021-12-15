459e6dc885cec769398ed7b8594329b8b86457c1
jinsung Park <jinsungpark@jinsungui-MacBookPro.local>
2021-12-15 21:05:01 +0900

    [ADD] Bookpage-view

--- books/views.py
+++ books/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.views         import View
 
-# Create your views here.
+from django.http.response import JsonResponse
+
+from django.db.models import Avg
+
+from .models              import Book
+from readybooks.settings  import ACCESS_KEY, ACCESS_SECRET_KEY
+
+
+class BookView(View):
+    def get(self, request, book_id):
+        try:
+            book    = Book.objects.prefetch_related('authorbook_set__author','review_set').get(id=book_id)
+            results = []
+
+            results.append({
+                "name"       : book.name,
+                "price"      : book.price,
+                "discription": book.description,
+                "thumbnail"  : book.thumbnail,
+                "authors"     : [author.author.name for author in book.authorbook_set.all()],
+                "average"    : book.review_set.all().aggregate(Avg('rating')),
+                "reviews"    : 
+                [{  "user"       : review.user.nickname,
+                    "rating"     : review.rating,
+                    "content"    : review.content,
+                    "created_at" : review.created_at
+                }for review in book.review_set.all()]
+            })
+            return JsonResponse({"result":results}, status=200)
+
+        except Book.DoesNotExist:
+            JsonResponse({"message" : "DOES_NOT_EXIST"}, status=401)
--- readybooks/settings.py
+++ readybooks/settings.py
@@ -21,8 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
-ALGORITHM  = os.environ['ALGORITHM']
+SECRET_KEY        = os.environ['SECRET_KEY']
+ALGORITHM         = os.environ['ALGORITHM']
+ACCESS_KEY        = os.environ['ACCESS_KEY']
+ACCESS_SECRET_KEY = os.environ['ACCESS_SECRET_KEY']
+REGION            = os.environ['REGION']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
