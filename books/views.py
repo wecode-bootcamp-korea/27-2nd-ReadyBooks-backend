83a14b371a82a90ef6a72efbf213c964da078d25
joshua <krjoshua21@gmail.com>
2021-12-16 02:11:26 +0900

    add avg rating sorted list api

--- books/views.py
+++ books/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http      import JsonResponse
+from django.views     import View
+from django.db.models import Avg
 
-# Create your views here.
+from books.models  import Review
+
+class BookRankingView(View):
+    def get(self, request):
+        
+        reviews = Review.objects.all().annotate(avg_rating=Avg('rating')).order_by('-avg_rating')[:10]
+        
+        rank_list=[{
+            'title'     : review.book.name,
+            'thumbnail' : review.book.thumbnail,
+            'rating'    : review.rating
+        } for review in reviews]
+
+        return JsonResponse({'result' : rank_list}, status = 200)
