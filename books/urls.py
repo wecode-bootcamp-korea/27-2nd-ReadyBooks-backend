0983a42fc43a7666cd92d022fbe800c43a1ab41b
joshua <krjoshua21@gmail.com>
2021-12-16 23:04:29 +0900

    add allbooks unit test

--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import BookView
+from .views import BookView,BooksView
 
 urlpatterns = [
-    path("/<int:book_id>",BookView.as_view())
+    path("/<int:book_id>",BookView.as_view()),
+    path('/main', BooksView.as_view()),
 ]
