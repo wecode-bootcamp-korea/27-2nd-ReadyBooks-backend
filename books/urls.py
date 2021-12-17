20c5b2dbb0ed431f3578ab246f6d993535873406
jinsung Park <jinsungpark@jinsungui-MacBookPro.local>
2021-12-17 18:53:56 +0900

    [ADD]댓글 Get 구현

--- books/urls.py
+++ books/urls.py
@@ -5,6 +5,6 @@ from .views import BookView,BooksView, ReviewView
 urlpatterns = [
     path("/<int:book_id>",BookView.as_view()),
     path('/main', BooksView.as_view()),
-    path("/review/<int:book_id>", ReviewView.as_view())
+    path("/review/<int:book_id>",ReviewView.as_view())
 ]
 
