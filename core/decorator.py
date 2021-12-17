6b0aaeaddc1f1f417eb1e52aa1a071f6b867ad88
JongHo Seong <jonghoseong@JongHoui-MacBookAir.local>
2021-12-17 17:18:05 +0900

    [MODIFY]login_decorator

--- core/decorator.py
+++ core/decorator.py
@@ -7,14 +7,17 @@ from users.models import User
 def login_required(func):
     def wrapper(self, request, *args, **kwargs):
         try:
-            token        = request.headers("Authorization")
+            token        = request.headers.get("Authorization",None)
+            if token == None:
+                request.user = token
+
+                return func(self, request, *args, **kwargs)
+
             payload      = jwt.decode(token, os.environ["SECRET_KEY"], algorithms=os.environ["ALGORITHM"])
             request.user = User.objects.get(id = payload["id"])
 
             return func(self, request, *args, **kwargs)
 
-        except KeyError:
-            return JsonResponse({"message":"KEY_ERROR"}, status = 400 )
         except User.DoesNotExist:
             return JsonResponse({"message":"INVALID_USER"}, status = 404 )
         except jwt.DecodeError:
