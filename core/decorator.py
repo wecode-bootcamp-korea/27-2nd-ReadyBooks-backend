f47d1bc890f7bbf0da18de1f94ce30a6a9d9b290
joshua <krjoshua21@gmail.com>
2021-12-23 16:45:01 +0900

    modified for front requests

--- core/decorator.py
+++ core/decorator.py
@@ -7,7 +7,7 @@ from users.models import User
 def login_required(func):
     def wrapper(self, request, *args, **kwargs):
         try:
-            token = request.headers.get("Authorization",None)
+            token = request.headers["Authorization"]
             payload      = jwt.decode(token, os.environ["SECRET_KEY"], algorithms=os.environ["ALGORITHM"])
             request.user = User.objects.get(id = payload["id"])
 
